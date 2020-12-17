5a87aeef3e858289e65b5f8c65826dfc01f9da49
AnnaKhoroshailo <annakhoroshaylo@gmail.com>
2020-12-17 10:22:53 +0200

    add dz function

--- pages/dz14/js/script.js
+++ pages/dz14/js/script.js
@@ -17,14 +17,44 @@ if(login=="ин") {
 console.log("Арифметическая прогрессия");
 let a1=3;
 let d=5;
-for (i=1; i<101; i++) {
-	an=a1+d*(i-1);
+for (n=1; n<101; n++) {
+	an=a1+d*(n-1);
 	console.log(an);
 }
 
+
+console.log("Способ проще");
+let a;
+do {
+	a=parseInt(prompt("Введите начальное значение"));
+} while (a !== a) //пока пользователь вводит нечисловое значение, выводить окно
+
+do {
+	step=parseInt(prompt("Введите шаг"));
+} while (step !== step)
+
+for (let k=0; k<100; k++) {
+	console.log(a)
+	a+=step;
+}
+ 
+console.log("--------------");
 
 console.log("Последовательность Фибоначчи");
 for (j=0; j<25; j++) {
 	fn=(Math.pow(((1+Math.sqrt(5))/2),j)-Math.pow(((1-Math.sqrt(5))/2),j))/Math.sqrt(5);
 	console.log(fn);
+}
+
+
+console.log("Способ проще");
+let n1=0;
+let n2=1;
+console.log(n1);
+console.log(n2);
+for (let z=0; z<23; z++) {
+	let sum=n1+n2;
+	n1=n2;
+	n2=sum;
+	console.log(n2);
 }
---
+++ pages/dz15/js/script.js
@@ -0,0 +1,72 @@
+console.log("Заглавная первая буква");
+function ucFirst(str) {
+	let first=str.charAt(0).toUpperCase();
+	let rest=str.slice(1)
+	return first+rest;
+}
+console.log(ucFirst("вася"));
+console.log(ucFirst(""));
+
+
+console.log("Максимальное из двух чисел");
+function max(a,b) {
+	return Math.max(a,b);
+}
+console.log(max(2, 5));
+console.log(max(3, -1));
+console.log(max(1, 1));
+
+
+console.log("10 случайных чисел (с помощью цикла) от первого до второго числа, что ввел пользователь");
+function random(first, second) {
+	let min=first;
+	let max=second;
+	if(min>max) { //меняем местами
+		let m=min;
+		min=max;
+		max=m;
+	}
+	for (i=0; i<10; i++) {
+		let num=Math.floor(Math.random()*(max-min+1))+min;
+		console.log(num);
+	}
+}
+let n1=parseInt(prompt("Введите минимальное значение"));
+let n2=parseInt(prompt("Введите максимальное значение"))
+random(n1,n2);
+
+
+console.log("Лотерея");
+function lottery() {	
+    let user_num1=parseInt(prompt("Введите 1 число от 1 до 36 для лотереи"));
+    let user_num2=parseInt(prompt("Введите 2 число от 1 до 36 для лотереи"));
+    let user_num3=parseInt(prompt("Введите 3 число от 1 до 36 для лотереи"));
+    let user_num4=parseInt(prompt("Введите 4 число от 1 до 36 для лотереи"));
+    let user_num5=parseInt(prompt("Введите 5 число от 1 до 36 для лотереи"));
+    let user_num6=parseInt(prompt("Введите 6 число от 1 до 36 для лотереи"));
+    let min=1;
+    let max=8;
+    let rusut=0;
+    let num="";
+    let verify;
+    let count=0;;
+    for (i=0; i<6; i++) {
+    	num+=Math.floor(Math.random()*(max-min+1))+min;
+    	if(num.split(", ")[i]==String(user_num1) || num.split(" ")[i]==String(user_num2) || num.split(" ")[i]==String(user_num3) || num.split(" ")[i]==String(user_num4) || num.split(" ")[i]==String(user_num5) || num.split(" ")[i]==String(user_num6)) {
+    		verify=true;
+    		count+=1;
+    	}
+    	else {
+    		verify=false;
+    	}
+    	num+="-"+verify;
+    	if(i<5) {
+    		num+=", ";
+    	}
+    }
+	console.log(`Числа, что вы ввели: ${user_num1}, ${user_num2}, ${user_num3}, ${user_num4}, ${user_num5}, ${user_num6}`);
+	console.log(`Числа, что вы выпали: ${num}`);
+	console.log(`Результат: ${count}/6`);
+}
+
+lottery();
